0ddc9e514e596d28d43c1be53d1d7f2fd247c180
zharfanzahisham <39366913+zharfanzahisham@users.noreply.github.com>
2018-12-16 17:09:10 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+repl.it
+@saifulanuar/MicroSecondJava-2
+HTML, CSS, JS
+No description
+
+fork
+run
+repl talk
+Sign up
+Files
+script.js
+history
+1234567891011121314151617181920212223242526272829303132
+
+
+// address of your smart contract deployed on the blockchain
+var smartContractAddress = "0x49E2b310F131C94E9AD8D88a8915a0FF6654cdD7";
+
+// ABI is a JSON formatted list of contract's function and arguments required to create the EVM bytecode required to call the function
+var abi = [
+	{
+		"constant": false,
+
+
+https://MicroSecondJava-2--saifulanuar.repl.co
+
+resultconsole
